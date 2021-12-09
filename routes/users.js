59330fe1538b8a87085679a4e73ac25f2b0dab94
Maria Eugenia Mariotti <eugeniamariotti93@gmail.com>
2021-12-08 21:00:17 -0300

    Adapted endpoints for user/pet registration. Missin tests.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passwordHasher = require('../utils/passwordHasher.js');
+var uuid = require('uuid');
 
 var http = require('http-status-codes');
 const db = require('../models/index.js');
@@ -50,22 +51,78 @@ router.get('/:userId', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-		console.log(`Creating user ${req.body.uuid}`);
-		db.Users.create({
-			uuid: req.body.uuid,
-			_ref: req.body._ref,
-			username: req.body.username,
-			password: passwordHasher(req.body.password),
-			email: req.body.email,
-			createdAt: new Date(),
-			updatedAt: new Date()
-		}).then((user) => {
-		    res.status(http.StatusCodes.CREATED).json(user); 
-		}).catch(err => {
-			console.error(err);
-			res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).send({ 
-			  error: http.getReasonPhrase(http.StatusCodes.INTERNAL_SERVER_ERROR) + ' ' + err 
+		return db.sequelize.transaction(function (t) {
+			//console.log(`Creating user ${JSON.stringify(req.body)}`);
+			db.Users.create({
+				uuid: req.body.uuid,
+				_ref: req.body._ref,
+				username: req.body.username,
+				password: passwordHasher(req.body.password),
+				email: req.body.email,
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}).then((user) => {
+				delete user['password'];
+
+				if (req.body.pets.length === 0) {
+					return;
+				}
+
+				// Create user pets
+				req.body.pets.forEach(pet => {
+					db.Pets.create({
+						uuid: pet.uuid,
+						_ref: pet._ref,
+						userId: user.uuid,
+						type: pet.type,
+						name: pet.name,
+						furColor: pet.furColor,
+						breed: pet.breed,
+						size: pet.size,
+						lifeStage: pet.lifeStage,
+						sex: pet.sex,
+						description: pet.description,
+						createdAt: new Date(),
+						updatedAt: new Date()
+					});
+					
+					if (typeof pet.photos === 'undefined' || pet.photos.length === 0) {
+						// return means continue in this context.
+						return;
+					}
+
+					console.log('Created pet successfully! Creating photos...');
+
+
+					// Add pet photos
+					pet.photos.forEach(petPhoto => {
+						db.Photos.create({
+							uuid: uuid.v4(),
+							photo: Buffer.from(petPhoto,'base64'),
+							createdAt: new Date(),
+							updatedAt: new Date()
+						}).then(createdPhoto => {
+							db.PetPhotos.create({
+								petId: pet.uuid,
+								photoId: createdPhoto.uuid,
+								createdAt: new Date(),
+								updatedAt: new Date()
+							})
+						});
+					});
+				});
 			});
+		}).then(function (result) {
+			// Transaction has been committed
+			// result is whatever the result of the promise chain returned to the transaction callback
+			createdUser = { uuid: req.body.uuid, _ref: req.body._ref, username: req.body.username}
+			console.log('User creation transaction completed successfully!');
+			return res.status(http.StatusCodes.CREATED).json(createdUser); 
+		}).catch(function (err) {
+			// Transaction has been rolled back
+			// err is whatever rejected the promise chain returned to the transaction callback
+			console.error('User creation transaction has been rolled back!');
+			throw err;
 		});
 	} catch (err) {
 		console.error(err);
