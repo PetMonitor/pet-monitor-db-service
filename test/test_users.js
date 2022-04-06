c07b65736cda43d0331c96b2f1451efc558ebf01
Maria Eugenia Mariotti <eugeniamariotti93@gmail.com>
2022-04-06 00:25:35 -0300

    Fix tests

--- test/test_users.js
+++ test/test_users.js
@@ -1,11 +1,36 @@
 const request = require('supertest');
 const server = require('../app');
-const { expect } = require('chai');
+const { expect, assert } = require('chai').use(require('chai-bytes'));
 var db = require('../models/index.js');
 var passwordHasher = require('../utils/passwordHasher.js');
 var fs = require('fs');
 
+var axios = require("axios");
+var MockAdapter = require("axios-mock-adapter");
+
+// This sets the mock adapter on the default instance
+var mock = new MockAdapter(axios);
+
+
 TEST_SEPARATOR = '=====================================================================';
+
+EMBEDDING = [ 
+  0.06037527,-0.01576912,0.07322315,-0.09460726,-0.08096360,-0.02301554,0.14367725,-0.08457291,
+  -0.01227567,-0.11083049,-0.06902093,-0.07299378,-0.14857993,-0.08143593,0.00886787,-0.12142590,
+  -0.05042028,-0.10201982,-0.02582854,-0.05871225,0.06041538,-0.03485662,-0.04477020,-0.10418238,
+  0.09800038,-0.05665335,-0.07040229,0.08284891,-0.10092543,0.02017560,-0.09319781,0.11097431,
+  -0.12110960,0.08574340,0.03695728,-0.11780335,-0.09819926,0.06415571,0.10743850,0.14196600,-0.12936668,
+  0.06096330,0.11253788,-0.13587150,-0.05594309,0.08005318,0.07479791,-0.03268669,-0.11030994,0.10099071,
+  -0.10825977,0.10710515,0.14553122,-0.10120714,0.06041265,0.06630402,0.11567818,-0.02630793,-0.04872397,
+  0.05499220,0.07236826,-0.04801438,-0.07588169,-0.11336783,0.07896858,-0.06593332,0.08287447,0.04932186,
+  -0.08283250,-0.07132198,0.15232562,-0.14693299,-0.00676774,-0.07884897,0.08104447,0.10064926,0.12246129,
+  0.07462891,-0.03215748,-0.07782545,0.10203280,0.08091006,0.05004479,-0.07561940,-0.05811106,-0.02652100,
+  0.05840707,-0.10823300,0.06568510,0.04405428,-0.10763825,0.05636177,0.10309774,-0.03507317,0.05172334,
+  0.08225141,-0.11055159,0.12676683,-0.08507260,0.05169133,0.09420424,-0.10848676,-0.05079362,-0.12370258,
+  -0.11680937,-0.08039150,-0.06842650,0.12966783,0.10653536,-0.03532178,-0.08595541,0.14921825,-0.00625481,
+  0.07807481,-0.01324250,-0.03535264,-0.14491458,0.09799559,0.15477061,0.08272006,0.05744857,-0.04393201,
+  -0.10136475,0.08224790,0.11506042,0.11539490,0.07415865,-0.10429314 
+]
 
 USERS = [
     {
@@ -79,7 +104,13 @@ PHOTOS = [
   }
 ]
 
-
+mock.onPost("http://host.docker.internal:5001/api/v0/dogs/embedding").reply(200, {
+  embeddings: [{ 
+    '126e4567-e89b-12d3-a456-426614176001': EMBEDDING,
+    '126e4567-e89b-12d3-a456-426614176002': EMBEDDING,
+    '126e4567-e89b-12d3-a456-426614176003': EMBEDDING
+  }],
+});
 
 describe('Users test case', function() {
 
@@ -269,10 +300,10 @@ describe('Users test case', function() {
     expect(pet1['dataValues']).to.deep.equal(PETS[1]);
 
     //Before comparing, field 'photo' must be changed back to base64
-    expect(Buffer.from(photo0['dataValues']['photo']).toString('base64')).to.equal(PHOTOS[0]['photo']);
-    expect(Buffer.from(photo1['dataValues']['photo']).toString('base64')).to.equal(PHOTOS[1]['photo']); 
-    expect(Buffer.from(photo2['dataValues']['photo']).toString('base64')).to.equal(PHOTOS[2]['photo']); 
-
+    expect(Buffer.from(photo0['dataValues']['photo'])).to.equalBytes(IMAGE_CONTENT_BLOB);
+    expect(Buffer.from(photo1['dataValues']['photo'])).to.equalBytes(IMAGE_CONTENT_BLOB);
+    expect(Buffer.from(photo2['dataValues']['photo'])).to.equalBytes(IMAGE_CONTENT_BLOB);
+    
     expect(pet0Photos[0]['dataValues']['photoId']).to.equal(PHOTOS[0]['uuid']);
     expect(pet0Photos[0]['dataValues']['petId']).to.equal(PETS[0]['uuid']);
 
