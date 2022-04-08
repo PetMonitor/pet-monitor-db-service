27773dbbb519edb935a2dbbd17b9f5aa489004ca
Camila de Trincheria <cdetrincheria@medallia.com>
2022-04-08 00:51:51 -0300

    Changes to support notice filters

--- routes/notices.js
+++ routes/notices.js
@@ -3,27 +3,59 @@ var router = express.Router();
 
 var http = require('http-status-codes');
 const db = require('../models/index.js');
+const {Op} = require("sequelize");
 
 /**
  * Notices CRUD endpoints.
  */
 
  router.get('/', async (req, res) => {
-	try {
-        db.Notices.findAll({
+	let queryParams = {}
+	if (req.query.petType != null) {
+		queryParams.petType = req.query.petType
+	}
+	if (req.query.petSex != null) {
+		queryParams.petSex = req.query.petSex
+	}
+	 if (req.query.noticeType != null) {
+		 queryParams.noticeType = req.query.noticeType.split(',')
+	 }
+
+	let petOptions = {
+		model: db.Pets,
+		required: true,
+		include: [{
+			model: db.PetPhotos,
+			required: true,
 			include: [{
-				model: db.Pets,
-				required: false,
-				include: [{
-					model: db.PetPhotos,
-					required: false,
-					include: [{
-						model: db.Photos,
-						required: false
-					}]
-				}]
+				model: db.Photos,
+				required: true
 			}]
-		}).then((notices) => {
+		}]
+	}
+	let noticeOptions = {}
+	if (!isEmptyObject(queryParams)) {
+		let where = {}
+		if (queryParams.petType != null && !isEmptyObject(queryParams.petType)) {
+			where.type = queryParams.petType
+		}
+		if (queryParams.petSex != null && !isEmptyObject(queryParams.petSex)) {
+			where.sex = queryParams.petSex
+		}
+		if (!isEmptyObject(where)) {
+			petOptions.where = where
+		}
+		if (queryParams.noticeType != null && !isEmptyObject(queryParams.noticeType)) {
+			noticeOptions.where = {
+				noticeType: {
+					[Op.or]: queryParams.noticeType
+				}
+			}
+		}
+	}
+	noticeOptions.include = [petOptions]
+	try {
+        db.Notices.findAll(noticeOptions).then((notices) => {
 			const resObj = notices.map(notice => {
 				return Object.assign({},{
 					uuid: notice.uuid,
@@ -55,5 +87,9 @@ const db = require('../models/index.js');
 	  });
   	} 
 });
+
+function isEmptyObject(obj) {
+	return !Object.keys(obj).length;
+}
 
 module.exports = router;
