652879f3bcb325ac2733d0b64bee14ea56ca6d27
Camila de Trincheria <cdetrincheria@medallia.com>
2022-05-18 21:49:11 -0300

    fix and refactor filters

--- routes/notices.js
+++ routes/notices.js
@@ -9,138 +9,31 @@ const { Op } = require("sequelize");
  * Notices CRUD endpoints.
  */
 
- router.get('/', async (req, res) => {
-	let queryParams = {}
-	if (req.query.petType != null) {
-		queryParams.petType = req.query.petType
-	}
-	if (req.query.petSex != null) {
-		queryParams.petSex = req.query.petSex
-	}
-	if (req.query.petBreed != null) {
-		queryParams.petBreed = req.query.petBreed
-	}
-	if (req.query.noticeType != null) {
-		queryParams.noticeType = req.query.noticeType.split(',')
-	}
-	if (req.query.noticeRegion != null) {
-		queryParams.noticeRegion = req.query.noticeRegion
-	}
-
+router.get('/', async (req, res) => {
 	let petOptions = {
 		model: db.Pets,
-		required: false,
+		required: true,
 		include: [{
 			model: db.PetPhotos,
-			required: false,
+			required: true,
 			include: [{
 				model: db.Photos,
 				required: false
 			}]
-		}]
+		}],
+		where: getPetFilterConditions(req.query)
 	}
 
-	 let noticeOptions = {}
-	 if (!isEmptyObject(queryParams)) {
-		let where = {}
-		if (queryParams.petType != null && !isEmptyObject(queryParams.petType)) {
-			where.type = queryParams.petType
-		}
-		if (queryParams.petSex != null && !isEmptyObject(queryParams.petSex)) {
-			where.sex = queryParams.petSex
-		}
-		if (queryParams.petBreed != null && !isEmptyObject(queryParams.petBreed)) {
-			where.breed = {
-				[Op.iLike]: queryParams.petBreed
-			}
-		}
-		if (!isEmptyObject(where)) {
-			petOptions.where = where
-		}
-
-		let noticeConditions = []
-		let noticeTypeCondition
-		let regionCondition
-		let withinRadiusCondition
-		if (queryParams.noticeType != null && !isEmptyObject(queryParams.noticeType)) {
-			noticeConditions.push({
-				noticeType: {
-					[Op.or]: queryParams.noticeType
-				}
-			})
-		}
-
-		if (queryParams.noticeRegion != null && !isEmptyObject(queryParams.noticeRegion)) {
-			noticeConditions.push({
-				[Op.or]: [
-					{
-						street: {
-							[Op.iLike]: `%${queryParams.noticeRegion}%`
-						}
-					},
-					{
-						neighbourhood: {
-							[Op.iLike]: `%${queryParams.noticeRegion}%`
-						}
-					},
-					{
-						locality: {
-							[Op.iLike]: `%${queryParams.noticeRegion}%`
-						}
-					},
-					{
-						country: {
-							[Op.iLike]: `%${queryParams.noticeRegion}%`
-						}
-					}
-				]
-			})
-		}
-
-		if (req.query.radiusKm != null && req.query.latitude != null && req.query.longitude != null) {
-			withinRadiusCondition = db.sequelize.fn(
-				'ST_DWithin',
-				db.sequelize.col('eventCoordinates'),
-				db.sequelize.fn('ST_GeographyFromText',
-					`POINT(${req.query.longitude} ${req.query.latitude})`),
-				parseInt(req.query.radiusKm) * 1000);
-			if (noticeConditions.length === 0) {
-
-			}
-		}
-
-		// noticeOptions.where = {
-		// 	[Op.and]: [
-		// 		noticeTypeCondition,
-		// 		regionCondition,
-		// 		db.sequelize.where(withinRadiusCondition, {[Op.is]: true})
-		// 	]}
-
+	let noticeOptions = {
+		include: [petOptions],
+		where: getNoticeFilterConditions(req.query)
 	}
-	noticeOptions.include = [petOptions]
+
 	try {
-        db.Notices.findAll(noticeOptions).then((notices) => {
+        db.Notices.findAll(noticeOptions).then(notices => {
 			const resObj = notices.map(notice => {
-				return Object.assign({},{
-					uuid: notice.uuid,
-					_ref: notice._ref,
-					userId: notice.userId,
-					noticeType: notice.noticeType,
-					eventLocationLat: notice.eventCoordinates.coordinates[1],
-					eventLocationLong: notice.eventCoordinates.coordinates[0],
-					street: notice.street,
-					neighbourhood: notice.neighbourhood,
-					locality: notice.locality,
-					country: notice.country,
-					description: notice.description,
-					eventTimestamp: notice.eventTimestamp,
-					createdAt: notice.createdAt,
-					updatedAt: notice.updatedAt,
-					petId: notice.petId,
-					petPhoto: notice.Pet.PetPhotos[0].Photo.lowResPhoto
-				})
+				return Object.assign({}, mapToDomain(notice))
 			});
-
             res.status(http.StatusCodes.OK).json(resObj);
         }).catch(err => {
 			console.error(err);
@@ -155,10 +48,6 @@ const { Op } = require("sequelize");
 	  });
   	} 
 });
-
-function isEmptyObject(object) {
-	return Object.keys(object).length === 0
-}
 
 router.get('/:noticeId', async (req, res) => {
 	try {
@@ -178,27 +67,8 @@ router.get('/:noticeId', async (req, res) => {
 					}]
 				}]
 			}]
-		}).then((notice) => {
-
-			const resObj = Object.assign({},{
-				uuid: notice.uuid,
-				_ref: notice._ref,
-				userId: notice.userId,
-				noticeType: notice.noticeType,
-				eventLocationLat: notice.eventCoordinates.coordinates[1],
-				eventLocationLong: notice.eventCoordinates.coordinates[0],
-				street: notice.street,
-				neighbourhood: notice.neighbourhood,
-				locality: notice.locality,
-				country: notice.country,
-				description: notice.description,
-				eventTimestamp: notice.eventTimestamp,
-				createdAt: notice.createdAt,
-				updatedAt: notice.updatedAt,
-				petId: notice.petId,
-				petPhoto: notice.Pet.PetPhotos[0].Photo.lowResPhoto
-			})
-
+		}).then(notice => {
+			const resObj = Object.assign({}, mapToDomain(notice))
 			res.status(http.StatusCodes.OK).json(resObj);
 		}).catch(err => {
 			console.error(err);
@@ -213,5 +83,108 @@ router.get('/:noticeId', async (req, res) => {
 	  });
   	}
 });
+
+function getPetFilterConditions(queryParams) {
+	let where = {}
+	if (!isEmptyObject(queryParams.petType)) {
+		where.type = queryParams.petType
+	}
+	if (!isEmptyObject(queryParams.petSex)) {
+		where.sex = queryParams.petSex
+	}
+	if (!isEmptyObject(queryParams.petBreed)) {
+		where.breed = {
+			[Op.iLike]: queryParams.petBreed
+		}
+	}
+	return where;
+}
+
+function getNoticeFilterConditions(queryParams) {
+	let noticeConditions = []
+
+	if (!isEmptyObject(queryParams.noticeType)) {
+		noticeConditions.push(getNoticeTypeFilter(queryParams))
+	}
+
+	if (!isEmptyObject(queryParams.noticeRegion)) {
+		noticeConditions.push(getNoticeRegionFilter(queryParams))
+	}
+
+	if (queryParams.radiusKm != null && queryParams.latitude != null && queryParams.longitude != null) {
+		noticeConditions.push(getNoticeLocationWithinRadiusFilter(queryParams))
+	}
+	return {[Op.and]: [...noticeConditions]};
+}
+
+function getNoticeTypeFilter(queryParams) {
+	return {
+		noticeType: {
+			[Op.or]: queryParams.noticeType.split(',')
+		}
+	};
+}
+
+function getNoticeRegionFilter(queryParams) {
+	return {
+		[Op.or]: [
+			{
+				street: {
+					[Op.iLike]: `%${queryParams.noticeRegion}%`
+				}
+			},
+			{
+				neighbourhood: {
+					[Op.iLike]: `%${queryParams.noticeRegion}%`
+				}
+			},
+			{
+				locality: {
+					[Op.iLike]: `%${queryParams.noticeRegion}%`
+				}
+			},
+			{
+				country: {
+					[Op.iLike]: `%${queryParams.noticeRegion}%`
+				}
+			}
+		]
+	};
+}
+
+function getNoticeLocationWithinRadiusFilter(queryParams) {
+	let withinRadiusCondition = db.sequelize.fn(
+		'ST_DWithin',
+		db.sequelize.col('eventCoordinates'),
+		db.sequelize.fn('ST_GeographyFromText',
+			`POINT(${queryParams.longitude} ${queryParams.latitude})`),
+		parseInt(queryParams.radiusKm) * 1000);
+	return db.sequelize.where(withinRadiusCondition, {[Op.is]: true});
+}
+
+function isEmptyObject(object) {
+	return object == null || Object.keys(object).length === 0
+}
+
+function mapToDomain(notice) {
+	return {
+		uuid: notice.uuid,
+		_ref: notice._ref,
+		userId: notice.userId,
+		noticeType: notice.noticeType,
+		eventLocationLat: notice.eventCoordinates.coordinates[1],
+		eventLocationLong: notice.eventCoordinates.coordinates[0],
+		street: notice.street,
+		neighbourhood: notice.neighbourhood,
+		locality: notice.locality,
+		country: notice.country,
+		description: notice.description,
+		eventTimestamp: notice.eventTimestamp,
+		createdAt: notice.createdAt,
+		updatedAt: notice.updatedAt,
+		petId: notice.petId,
+		petPhoto: notice.Pet.PetPhotos[0].Photo.lowResPhoto
+	};
+}
 
 module.exports = router;
