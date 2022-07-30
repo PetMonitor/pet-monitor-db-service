11e9a897e3f3d47249f8b16b9c8e9e8edca3ca14
Maria Eugenia Mariotti <eugeniamariotti93@gmail.com>
2022-07-29 23:28:53 -0300

    Add endpoint to send messages to other users

--- routes/emails.js
+++ routes/emails.js
@@ -1,10 +1,12 @@
 var express = require('express');
+
+const fs = require('fs');
+const path = require('path');
+const db = require('../models/index.js');
 var http = require('http-status-codes');
 const emailUtils = require('../utils/emails.js');
 const common = require('../utils/common.js');
 const cache = require('../cache/cache.js');
-const path = require('path');
-const fs = require('fs');
 
 var router = express.Router({ mergeParams: true });
 const logger = require('../utils/logger.js');
@@ -12,6 +14,8 @@ const logger = require('../utils/logger.js');
 const port = process.env.PORT || '8000';
 
 const CONFIRMATION_EMAIL_SUBJECT = 'Pet Monitor: Confirmá tu email!';
+const USER_TO_USER_EMAIL_SUBJECT = 'Pet Monitor: Recibiste un mensaje!'
+
 const CONFIRMATION_LINK = `http://localhost:${port}/emails/confirmation`;
 const EMAIL_TOKEN_PREFIX = 'emailConfirmationToken:';
 
@@ -23,9 +27,59 @@ const EMAIL_TOKEN_LENGTH_BYTES = 48
 * Email endpoints.
 */
 
-router.post('/confirmation', async (req, res) => {
+router.post('/', async (req, res) => {
+    const sendTo = req.body.sendTo;
+    const message = req.body.message;
+    const senderContactEmail = req.body.contactEmail;
+    const senderContactPhoneNumber = req.body.contactPhoneNumber;
+
     try {  	
-        const emailAddress = req.body.emailAddress;
+        if (senderContactEmail.length == 0 && senderContactPhoneNumber.length == 0) {
+            const errorMessage = 'Must provide at least one of "senderContactEmail" or "senderContactPhoneNumber"';
+            logger.error(errorMessage);
+            return res.status(http.StatusCodes.BAD_REQUEST).send({ 
+                error: errorMessage
+            });
+        }
+
+        const recipient = await db.Users.findOne({ where: { email: sendTo } })
+        logger.info(`Recipient ${recipient}`);
+
+        if (recipient == null) {
+            const errorMessage = `Unable to find user with email address ${sendTo}`;
+            logger.error(errorMessage);
+            return res.status(http.StatusCodes.NOT_FOUND).send({ 
+                error: errorMessage
+            });
+        }
+
+        await emailUtils.sendEmail(
+            sendTo, 
+            USER_TO_USER_EMAIL_SUBJECT, 
+            '../static/userToUserEmailTemplate.html',
+            { 
+                "recipientUsername" : recipient.username,
+                "message" : message,
+                "senderContactEmail" : senderContactEmail.length > 0 ? senderContactEmail : '-',
+                "senderContactPhone" : senderContactPhoneNumber.length > 0 ? senderContactPhoneNumber : '-',
+            }
+        );
+        
+        logger.debug(`Sending email to ${sendTo}.`);
+
+        res.status(http.StatusCodes.CREATED).send();
+    } catch (err) {
+        logger.error(`Failed to send email to user ${sendTo}: ${err}`)
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({ 
+          error: http.getReasonPhrase(http.StatusCodes.INTERNAL_SERVER_ERROR) + ' ' + err 
+        });
+    }
+})
+
+router.post('/confirmation', async (req, res) => {
+    const emailAddress = req.body.emailAddress;
+
+    try {  	
         const emailConfirmationToken = common.generateSecureToken(EMAIL_TOKEN_LENGTH_BYTES);
 
         await emailUtils.sendEmail(
@@ -44,7 +98,7 @@ router.post('/confirmation', async (req, res) => {
         res.status(http.StatusCodes.CREATED).send();
     } catch (err) {
         logger.error(`Failed to send confirmation email for user ${emailAddress}: ${err}`)
-        res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).send({ 
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({ 
           error: http.getReasonPhrase(http.StatusCodes.INTERNAL_SERVER_ERROR) + ' ' + err 
         });
     }
@@ -69,10 +123,10 @@ router.get('/confirmation/:confirmationToken', async (req, res) => {
         cache.set(emailAddress, true, EMAIL_CONFIRMED_TTL_SECONDS);
 
         var emailConfirmedView = fs.readFileSync(path.resolve(__dirname, '../static/emailVerifiedView.html'), 'utf8');
-        res.send(emailConfirmedView);
+        return res.send(emailConfirmedView);
     } catch (err) {
         logger.error(`Failed to confirm email for session token ${req.params.confirmationToken}: ${err}`)
-        res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).send({ 
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({ 
           error: http.getReasonPhrase(http.StatusCodes.INTERNAL_SERVER_ERROR) + ' ' + err 
         });        
     }
@@ -83,13 +137,13 @@ router.post('/confirmation/check', async (req, res) => {
         const emailAddress = req.body.emailAddress;
         const emailConfirmed = cache.get(emailAddress);
 
-        res.status(http.StatusCodes.OK).send({ 
+        return res.status(http.StatusCodes.OK).send({ 
             confirmed: emailConfirmed != undefined
         });  
 
     } catch (error) {
         logger.error(`Failed to check if email ${req.body.emailAddress} was confirmed: ${err}`)
-        res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).send({ 
+        return res.status(http.StatusCodes.INTERNAL_SERVER_ERROR).json({ 
           error: http.getReasonPhrase(http.StatusCodes.INTERNAL_SERVER_ERROR) + ' ' + err 
         });        
     }
